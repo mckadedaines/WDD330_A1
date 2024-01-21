190ae7fced2b2377b1ece2ade73ce0ec9ad9d39b
lvisnaw <lvisnaw@gmail.com>
2024-01-20 19:40:37 -0700

    Fix cart add

--- src/js/product.js
+++ src/js/product.js
@@ -1,16 +1,31 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs"; // Add this line
 import { findProductById } from "./productData.mjs";
-
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
 
 // add listener to Add to Cart button
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
+
+  function addProductToCart(product) {
+    let cartItems = getLocalStorage("so-cart");
+
+    if (!Array.isArray(cartItems)) {
+        cartItems = [];
+    }
+
+    cartItems.push(product);
+    setLocalStorage("so-cart", cartItems);
+}
+
+// add to cart button event handler
+async function addToCartHandler(e) {
+  console.log("Triggerd")
+  try {
+    const product = await findProductById(e.target.dataset.id);
+    addProductToCart(product);
+    alert("Product added to cart successfully!");
+  } catch (error) {
+    console.error("Error adding product to cart:", error);
+    alert("There was an error adding the product to the cart.");
+  }
+}
