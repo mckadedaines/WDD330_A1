d5b288339db4df3cf07ca54202b627632867296b
mckadedaines <mckadedaines1@gmail.com>
2024-01-26 14:37:45 -0700

    Completed the Team Activity 2. The website items will now render based off of the info inside of the JSON file. Product.js was gutted and mainly moved over into the new file PoductDetails.mjs.

--- src/js/product.js
+++ src/js/product.js
@@ -1,37 +1,8 @@
+import { getParam } from "./utils.mjs";
+import productDetails from "./productDetails.mjs";
 
-import { setLocalStorage, getLocalStorage, getParam } from "./utils.mjs"; // Add this line
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
-import { findProductById } from "./productData.mjs";
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
-
-  function addProductToCart(product) {
-    let cartItems = getLocalStorage("so-cart");
-
-    if (!Array.isArray(cartItems)) {
-        cartItems = [];
-    }
-
-    cartItems.push(product);
-    setLocalStorage("so-cart", cartItems);
-}
-
-// add to cart button event handler
-async function addToCartHandler(e) {
-  console.log("Triggerd")
-  try {
-    const product = await findProductById(e.target.dataset.id);
-    addProductToCart(product);
-    alert("Product added to cart successfully!");
-  } catch (error) {
-    console.error("Error adding product to cart:", error);
-    alert("There was an error adding the product to the cart.");
-  }
-
-}
-
+// Get the product ID from the URL
 const productId = getParam("product");
-console.log(findProductById(productId));
+
+// Initialize product details functionality
+productDetails(productId);
