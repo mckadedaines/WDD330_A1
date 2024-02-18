720288fe1495ec12446f6b8d4d6f29b2fb236546
lvisnaw <lvisnaw@gmail.com>
2024-02-17 18:46:14 -0700

    Items are showing in categories now.

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -27,8 +27,9 @@ export function setClick(selector, callback) {
 export function getParam(param) {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-  const product = urlParams.get("product");
-  return product;
+  // const product = urlParams.get("product"); This is not in the instructors example
+  // return product;
+  return urlParams.get(param);
 }
 
 export function renderListWithTemplate(
