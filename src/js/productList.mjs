650e5a7364fec6f01dad13db05d1f58cac312e8c
DANIEL GERDES <ger19007@byui.edu>
2024-02-21 20:52:47 -0800

    all js files

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -1,41 +1,26 @@
-import { getData } from "./productData.mjs";
+import { getProductsByCategory } from "./externalServices.mjs";
 import { renderListWithTemplate } from "./utils.mjs";
 
 function productCardTemplate(product) {
-    return `<li class="product-card">
-        <a href="/product_pages/index.html?product=${product.Id}">
-            <img
-                src="${product.Images.PrimaryMedium}" 
-                alt="Image of ${product.Name}"/>
-            <h3 class="card__brand">${product.Brand.Name}</h3>
-            <h2 class="card__name">${product.NameWithoutBrand}</h2>
-            <p class="product-card__price">$${product.FinalPrice}</p>
-        </a>
-    </li>`;
+  return `<li class="product-card">
+    <a href="/product_pages/index.html?product=${product.Id}">
+    <img
+      src="${product.Images.PrimaryMedium}"
+      alt="Image of ${product.Name}"
+    />
+    <h3 class="card__brand">${product.Brand.Name}</h3>
+    <h2 class="card__name">${product.NameWithoutBrand}</h2>
+    <p class="product-card__price">$${product.FinalPrice}</p></a>
+  </li>`;
 }
 
-// Code to determine if image is broken, if broken then do not display product.
-// async function filterProducts(products) {
-//     const filteredProducts = [];
-//     for (const product of products) {
-//         try {
-//             // Check if image returns a valid response
-//             const response = await fetch(product.Image);
-//             if (response.ok) {
-//                 filteredProducts.push(product);
-//             }
-//         } catch (error) {
-//             console.error("Error checking image:", error);
-//         }
-//     }
-//     return filteredProducts;
-// }
-
 export default async function productList(selector, category) {
-    const el = document.querySelector(selector);
-    const products = await productList(category);
-    // const filteredProducts = await filterProducts(products);
-    console.log(products);
-    renderListWithTemplate(productCardTemplate, el, products);
-    document.querySelector(".title").innerHTML = category;
+  // get the element we will insert the list into from the selector
+  const el = document.querySelector(selector);
+  // get the list of products
+  const products = await getProductsByCategory(category);
+  console.log(products);
+  // render out the product list to the element
+  renderListWithTemplate(productCardTemplate, el, products);
+  document.querySelector(".title").innerHTML = category;
 }
