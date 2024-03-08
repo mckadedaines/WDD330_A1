b74baa49b1eac6550e2f7b9167851298facb9b2a
lvisnaw <lvisnaw@gmail.com>
2024-03-07 20:33:07 -0700

    Fixed cart count

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,4 @@ import productList from "./productList.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
-productList(".product-list", "tents");
+// productList(".product-list", "tents"); // this should not be here.
